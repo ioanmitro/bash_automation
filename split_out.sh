c04a668cc045c56a66d470f5323755df945cef6e
ioanmitro <38588891+ioanmitro@users.noreply.github.com>
2022-07-15 15:44:00 +0300

    Update split_out.sh

--- split_out.sh
+++ split_out.sh
@@ -3,25 +3,19 @@
 # The output will be split only if the input exceedsnout limit, but the split command 
 # always creates at least one new file
 
-#The following code illustrates a way to break up input into fixed sizes only
-#if the input exceeds the size limit
+# The following code illustrates a way to break up input into fixed sizes only
+# if the input exceeds the size limit
 
-
-#Output fixed-size pieces of input ONLY if the limit is exceeded 
-# Called like: Split <file> <prefix> <limit option> <limit argument>
-# e.g. Split $output ${output} 
-
+# Function of fixed-size pieces split when the limit is exceeded 
 function split {
 
-	local file = $1
+	local myfile = $1
 	local prfx = $2
 	local limit_tp = $3
 	local limit_sz = $4
 	local wc_option
-
-
-	# Sanity checks 
-	if [ -z "$file" ]; then
+ 
+	if [ -z "$myfile" ]; then
 		printf "%b" "Split: requires a valid file name!\n"
 		return 1
 	fi 
@@ -39,20 +33,18 @@ function split {
 		printf "%b" "Split: requires a limit size!\n"
 		return 1
 	fi
-
-#Convert split options to wc options. Sigh
-# Not all options supported by all wc/splits on all systems
-
+	
+	# Split options to wc options
 	case $limit_tp in 
 		-b|--bytes) 		wc_option='-c';;
 		-c|--line-bytes)	wc_option='-L';;
 		-l|--lines)		wc_option='-l';;
 	esac 
 
-#If the limit is exceeded 
+	# If limit size exceeded 
 	if [ "$(wc $wc_option $file | awk '{print $1}')" -gt $limit_sz ]; then
-	# Actually do something
-		split --verbose $limit_tp $limit_sz $file $prfx
+		# Split into pieces
+		split --verbose $limit_tp $limit_sz $myfile $prfx
 	fi 
 
 }
